855a8b8b74008a54540d81613ef096b0a6c64af1
Glenn Turner <glennturner@Glenns-iMac.broadband>
2019-02-16 16:07:48 +0000

    Fixed missing semi colon

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var handleRequest = function( request, response) {
     response.writeHead(200);
     response.end("hello Kubernetes World!");
 
-}
+};
 var wwww = http.createServer(handleRequest);
 wwww.listen(8080);
